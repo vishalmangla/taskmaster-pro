8b1f8936199083c868f130e5935d9f01724f0764
Vishal Mangla <mangla@gmail.com>
2022-02-25 14:25:15 -0800

    initial

--- assets/js/script.js
+++ assets/js/script.js
@@ -175,3 +175,49 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+   // console.log("activate", this);
+  },
+  deactivate: function(event){
+   // console.log("deactivate", this);
+  },
+  over: function(event) {
+   // console.log("over", event.target);
+  },
+  out: function(event) {
+   // console.log("out", event.target);
+  },
+  update: function(event) {
+
+    //array to store the tast data in
+    var tempArr =[];
+    //loop over current set of children in sortable list
+    //console.log($(this).children());
+    $(this).children().each(function(){
+
+      var text = $(this).find("p").text().trim();
+      var date = $(this).find("span").text().trim();
+      //add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+      
+    });
+    //trim down lust's ID to match object property
+    var arrName = $(this)
+    .attr("id").replace("lit-","");
+
+    //update array on tasts object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+    console.log(tasks);
+  }
+  
+});
+
